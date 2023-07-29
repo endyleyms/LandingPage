70946470412fe5dce5d840cb34fe76993c806d2c
Wendy Medina <endyleyms@gmail.com>
2023-07-29 08:33:38 -0500

    sytles fixing

--- src/components/AssociationCard/AssociationCard.tsx
+++ src/components/AssociationCard/AssociationCard.tsx
@@ -4,7 +4,7 @@ import Card from 'react-bootstrap/Card';
 const AssociationCard = (props: { CardImg: string, description: string }) => {
   const { CardImg = '', description = '' } = props
   return (
-    <Card className='card-container'>
+    <Card className='card--association-container'>
       <Card.Img variant="top" src={CardImg} className='card-img-container' />
       <Card.Body>
         <Card.Text className='card-text'>
